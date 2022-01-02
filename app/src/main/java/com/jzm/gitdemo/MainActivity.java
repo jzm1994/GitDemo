10173db8ff45bc4d4c7673ac51fd8c2ccd1b9a75
jinzm <zm.jin@qq.com>
2022-01-02 11:46:59 +0800

    add viewModel create

--- app/src/main/java/com/jzm/gitdemo/MainActivity.java
+++ app/src/main/java/com/jzm/gitdemo/MainActivity.java
@@ -1,14 +1,20 @@
 package com.jzm.gitdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyViewModel myViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
     }
 }
