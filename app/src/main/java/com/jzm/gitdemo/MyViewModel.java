06c7d32adbd6ca6f7e48576c03daae8149e520a9
jinzm <zm.jin@qq.com>
2022-01-02 12:05:44 +0800

    add viewModel number

--- app/src/main/java/com/jzm/gitdemo/MyViewModel.java
+++ app/src/main/java/com/jzm/gitdemo/MyViewModel.java
@@ -1,11 +1,30 @@
 package com.jzm.gitdemo;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
 
+    private MutableLiveData<Integer> mNumberLiveData = new MutableLiveData<>();
+    private int number;
+
     public MyViewModel() {
         super();
+    }
+
+    public void add() {
+        number ++;
+        mNumberLiveData.setValue(number);
+    }
+
+    public void sub() {
+        number --;
+        mNumberLiveData.setValue(number);
+    }
+
+    public void reset() {
+        number = 0;
+        mNumberLiveData.setValue(number);
     }
 
 }
